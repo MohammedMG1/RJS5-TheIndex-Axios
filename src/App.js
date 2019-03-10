3291c90c00eba979fb9bcd1a0f371f20ffed3184
Mohammed Alotaibi <imohammed.mg@gmail.com>
2019-03-10 13:55:52 +0300

    Mohammed Alotaibi

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React, { Component } from "react";
+import axios from 'axios';
 
-import authors from "./data.js";
 
 // Components
 import Sidebar from "./Sidebar";
@@ -10,16 +10,40 @@ import AuthorDetail from "./AuthorDetail";
 class App extends Component {
   state = {
     currentAuthor: null,
-    filteredAuthors: authors
+    filteredAuthors: [],
+    authors: [],
+    loading: true,
+    error: null
   };
+  async componentDidMount() {
+    try {
+      const response = await axios.get("https://the-index-api.herokuapp.com/api/authors/");
+      const authors = response.data;
+      this.setState({ authors: authors, filteredAuthors: authors, loading: false });
+    } catch (error) {
+      console.log("something went wrong ");
+      console.log(error);
+      this.setState({ erroe: error });
+    }
+  }
 
-  selectAuthor = author => this.setState({ currentAuthor: author });
+  selectAuthor = async (author) => {
+    try {
+      const response = await axios.get(`https://the-index-api.herokuapp.com/api/authors/${author.id}/`);
+      const newAuthor = response.data;
+      this.setState({ currentAuthor: newAuthor });
+    } catch (error) {
+      console.log("something went wrong ");
+      console.log(error);
+      this.setState({ erroe: error });
+    }
+  };
 
   unselectAuthor = () => this.setState({ currentAuthor: null });
 
   filterAuthors = query => {
     query = query.toLowerCase();
-    let filteredAuthors = authors.filter(author => {
+    let filteredAuthors = this.state.authors.filter(author => {
       return `${author.first_name} ${author.last_name}`
         .toLowerCase()
         .includes(query);
@@ -42,6 +66,8 @@ class App extends Component {
   };
 
   render() {
+    if (this.state.error) return <div>somthing went wrong! </div>;
+    if (this.state.loading) return <div>loading...</div>
     return (
       <div id="app" className="container-fluid">
         <div className="row">
